38cd64428d7e3ca0abfec751ef613242836f0925
Dani025a <dani025a@edu.easj.dk>
2024-05-02 20:25:44 +0200

    publishPaymentCaptured changed

--- messages/publishPaymentCaptured.js
+++ messages/publishPaymentCaptured.js
@@ -1,21 +1,27 @@
 import connectToRabbitMQ from './connection.js';
 
 async function publishPaymentCaptured(order) {
-        try {
-            const connection = await connectToRabbitMQ();
-            const channel = await connection.createChannel();
-            const exchange = 'product';
-            const routingKey = 'payment_captured';
+    try {
+        const connection = await connectToRabbitMQ();
+        const channel = await connection.createChannel();
+        const exchange = 'product';
+        const queueName = 'payment_captured';
 
-            await channel.assertExchange(exchange, 'direct', {
-                durable: true
-            });
+        await channel.assertExchange(exchange, 'direct', {
+            durable: true
+        });
 
-            channel.publish(exchange, routingKey, Buffer.from(order));
-            console.log('payment_captured message published successfully');
-        } catch (error) {
-            console.error('Error publishing payment_captured message:', error);
-        }
+        await channel.assertQueue(queueName, {
+            durable: true
+        });
+
+        await channel.bindQueue(queueName, exchange, 'payment_captured');
+
+        channel.publish(exchange, 'payment_captured', Buffer.from(order));
+        console.log('payment_captured message published successfully');
+    } catch (error) {
+        console.error('Error publishing payment_captured message:', error);
+    }
 }
 
 export default publishPaymentCaptured;
